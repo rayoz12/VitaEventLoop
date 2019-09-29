9656f66bdedcf6d5a8c55c2fdb853da70eed1100
Ryan Pereira <rayoz12@yahoo.com.au>
2019-09-30 08:15:10 +1000

    Started work on Node.js C++ API

--- src/platform/net/udp.h
+++ src/platform/net/udp.h
@@ -6,17 +6,40 @@
 namespace Platform
 {
 
+// State to 
+enum SocketState {
+    INITIAL,
+    BINDED,
+    CONNECTED
+};
+
+struct RemoteAddress {
+    std::string address;
+    std::string family;
+    int port;
+    int size;
+};
+
+using RemoteAddress_t = struct RemoteAddress;
+
+using CloseCallback=std::function<void()>;
+using ConnectCallback=std::function<void()>;
+using ErrorCallback=std::function<void(std::string)>;
+using ListeningCallback=std::function<void()>;
+using MessageCallback=std::function<void(void* buf, size_t len, RemoteAddress_t rinfo)>;
+
 class UDPSocket : EventEmitter
 {
 public:
 	UDPSocket();
 
-    void bind(int port)
+    void bind(int port, std::string address, const ListeningCallback& f = {});
+    void bind(int port, std::string address, const ListeningCallback& f = {});
 
 private:
 	bool isServerSocket = false;
     bool isOpen = false;
-
+    SocketState SocketState;
 };
 
 } // namespace Platform
